38d4b7929de92dd89b68ee51738b39ab7a41541c
Saurabh <gohainsaurabh@gmail.com>
2022-10-07 20:06:39 +0530

    Add some pattern printing code

---
+++ c++/pattern6.cpp
@@ -0,0 +1,21 @@
+// Floyd's triangle
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int c = 1;
+    for (int i = 1; i <= n; i++)
+    {
+      for(int j = 1; j <= i; j++){
+          cout<<c<<" ";
+          c++;
+      }
+
+        cout << endl;
+    }
+    return 0;
+}
